911fb6e7c49b1f87a5472011fbfd451dff1fcb59
Dexter49 <dexterousguru49@yahoo.com>
2026-09-20 20:02:24 +0100

    Defined the theme colors and styles

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  darkTheme: "#000",
+  lightTheme: "#fff",
+  lightGold: "#c9b48d",
+  mainGold: "#bc8d32",
+  supportingGold: "#dba524",
+  platformGrey: "#797875",
+  buttonMain: "#ad6f15",
+};
+
+export const SIZES = {
+  padding: 24,
+  borderRadius: 16,
+};
